dcba47455d1d0ed5e4222fa040dec91633314f1c
laowang <wangxin@itcast.cn>
2021-12-12 16:23:06 +0800

    new test class

---
+++ demo1212/src/main/java/com/example/demo/test/Mytest.java
@@ -0,0 +1,12 @@
+package com.example.demo.test;
+
+/**
+ * @author NickWen
+ * @create 2021/12/12
+ **/
+public class Mytest {
+
+    public static void main(String[] args) {
+        System.out.println("this is a project being committed to github!!");
+    }
+}
